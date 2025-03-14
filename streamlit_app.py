736d577f2f8e8d373041f5d216fe219104a9efc1
DhiaShafa05 <dhia.shafa06@gmail.com>
2025-03-14 11:33:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ st.write(
     "Let's play the game, try to make new innovation"
 )
 #Data bahan baku (nama, kandungan gizi, biaya, tingkat inovasi)
+bahan_baku = {
+    "Kacang Hijau": {"protein": 8, "serat": 7, "biaya": 5, "inovasi": 6},
+    "ubi ungu": {"protein": 4, "serat": 8, "biaya": 4, "inovasi": 7},
+    "keju": {"protein": 9, "serat": 2, "biaya": 8, "inovasi": 5},
+    "Rumput Laut": {"protein": 6,"serat": 9, "biaya": 6, "inovasi": 8}
+}
